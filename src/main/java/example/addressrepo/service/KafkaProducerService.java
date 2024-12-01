0080c118cdd7afb211e66921d0de8209fb84e92c
arhdez87 <arhdez87@gmail.com>
2024-12-01 09:08:18 -0600

    first Kafka test working

---
+++ src/main/java/example/addressrepo/service/KafkaProducerService.java
@@ -0,0 +1,16 @@
+package example.addressrepo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class KafkaProducerService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message){
+        kafkaTemplate.send(topic, message);
+        System.out.println("Message sent to topic " + topic + ": " + message);
+    }
+}
